93701555acec770a7ba7afa92c9a16e9194c36c5
Changsu Kang <changsu.kang@outlook.com>
2018-09-07 11:14:34 +0000

    send memory/swap info.

--- zbx_lxc.sh
+++ zbx_lxc.sh
@@ -2,7 +2,13 @@
 
 . $(dirname $0)/env.sh # loads zsender, zserver and zport
 
-objects="cpuacct.usage"
+objects=( \
+	'cpuacct.usage' \
+	'memory.usage_in_bytes' \
+	'memory.limit_in_bytes' \
+	'memory.memsw.usage_in_bytes' \
+	'memory.memsw.limit_in_bytes' \
+)
 
 discovery="$(dirname $0)/discovery"
 cgroup="$(dirname $0)/cgroup"
@@ -23,9 +29,29 @@ zabbix_sender -vv -z $zserver -p $zport -i $discovery
 
 echo -n "" > $cgroup 
 for c in ${active[@]}; do
-    for o in $objects; do
-	echo "$zsender lxc.$o[$c] $(echo $(lxc-cgroup -n $c $o)/1000/1000/1000 | bc -l)" >> $cgroup
+    for o in ${objects[*]}; do
+	echo -n "$zsender lxc.$o[$c] " >> $cgroup
+	if [ "$o" == "cpuacct.usage" ]; then
+	    value=$(echo "scale=9; $(lxc-cgroup -n $c $o)/1000/1000/1000" | bc -l)
+	else
+	    value=$(echo "scale=9; $(lxc-cgroup -n $c $o)/1024/1024/1024" | bc -l)
+	    if [ "$o" == "memory.usage_in_bytes" ]; then
+		memusage=$value
+	    elif [ "$o" == "memory.limit_in_bytes" ]; then
+		memlimit=$value
+	    elif [ "$o" == "memory.memsw.usage_in_bytes" ]; then
+		swusage=$value
+	    elif [ "$o" == "memory.memsw.limit_in_bytes" ]; then
+		swlimit=$value
+	    fi
+	fi
+	echo $value >> $cgroup
     done
+    # calculate percentage
+    mempercent=$(echo "scale=2; 100*$memusage/$memlimit" | bc -l)
+    echo "$zsender lxc.memory.usage_in_percent[$c] $mempercent" >> $cgroup
+    swpercent=$(echo "scale=2; 100*$swusage/$swlimit" | bc -l)
+    echo "$zsender lxc.memory.memsw.usage_in_percent[$c] $swpercent" >> $cgroup
 done
 
 zabbix_sender -vv -z $zserver -p $zport -i $cgroup
